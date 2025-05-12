4e687e5efb668807a23577c06eced5a056342fc1
Ramadhan <whyramadhan27@gmail.com>
2025-05-12 11:41:44 +0700

    add: adding select by lang features

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -7,6 +7,13 @@ const baseUrl = import.meta.env.VITE_API_URL;
 const [person, setPerson] = createSignal("PenyuDev");
 const [quote, setQuote] = createSignal("Klik untuk mendapatkan kutipan");
 
+const [Language, SetLanguage] = createSignal("id");
+
+const HandleLanguage = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  SetLanguage(target.value);
+};
+
 // const DataType = {
 //   person: String,
 //   quote: String,
@@ -29,7 +36,7 @@ const fetchData = async () => {
   try {
     const response = await axios({
       method: "GET",
-      url: `${baseUrl}/quote?lang=id`,
+      url: `${baseUrl}/quote?lang=${Language()}`,
       withCredentials: false,
       headers: {
         "Content-Type": "application/json",
@@ -38,6 +45,7 @@ const fetchData = async () => {
 
     setPerson(response.data.person);
     setQuote(response.data.quote);
+    console.log(Language());
   } catch (error) {
     console.error("Error fetching data:", error);
     showToast("Failed to fetch data!", "error");
@@ -71,6 +79,23 @@ const Hero = () => {
           <p>{item}</p>
         </div>
       ))} */}
+      <div class="text-white max-w-md mx-auto mt-6">
+        <label for="language" class="block text-sm font-medium text-white mb-2">
+          Pilih Bahasa:
+        </label>
+        <select
+          id="language"
+          value={Language()}
+          onChange={HandleLanguage}
+          class="block w-full rounded-xl border border-gray-300 bg-gray-800 text-white py-2 px-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        >
+          <option value="">-- Pilih Bahasa --</option>
+          <option value="id">Bahasa Indonesia</option>
+          <option value="Sundanese">Sunda</option>
+          <option value="Javanese">Jawa</option>
+        </select>
+      </div>
+
       <div class="flex gap-4 mt-4">
         <button
           onClick={() => copyToClipboard(quote())}
